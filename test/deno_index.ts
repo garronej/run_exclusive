22999ef53503406f3bba3b75e6916808a2a12782
denoify_ci <denoify_ci@github.com>
2020-05-21 15:06:21 +0000

    🚀

---
+++ test/deno_index.ts
@@ -0,0 +1,23 @@
+
+declare const console: any;
+
+console.log("DENO TEST");
+
+import "./test1.ts";
+import "./test2.ts";
+import "./test3.ts";
+import "./test4.ts";
+import "./test5.ts";
+import "./test6.ts";
+import "./test7.ts";
+import "./test8.ts";
+import "./test9.ts";
+import "./test10.ts";
+import "./test11.ts";
+
+import "./test13.ts";
+import "./test14.ts";
+import "./test15.ts";
+import "./test16.ts";
+import "./test17.ts";
+import "./test18.ts";
---
+++ test/legacyTests/test2.ts
@@ -0,0 +1,61 @@
+import * as runExclusive from "../../lib/runExclusive.ts";
+
+class MyClass {
+
+    constructor() { };
+
+    private alphabetStack = "";
+
+    private groupRef = runExclusive.createGroupRef();
+
+    public myMethodUpperCase = runExclusive.buildMethodCb(
+        this.groupRef,
+        (char: string, callback?: (alphabet: string) => void): void => {
+
+            setTimeout(() => {
+                this.alphabetStack += char.toUpperCase();
+                callback!(this.alphabetStack);
+            }, Math.random() * 1000);
+
+        }
+    );
+
+
+    public myMethod = runExclusive.buildMethodCb(
+        this.groupRef,
+        (char: string, callback?: (alphabet: string) => void): void => {
+
+            setTimeout(() => {
+                this.alphabetStack += char;
+                callback!(this.alphabetStack);
+            }, Math.random() * 1000);
+
+        }
+    );
+
+
+}
+
+let inst = new MyClass();
+
+
+inst.myMethod("a");
+
+for (let char of ["b", "c", "d", "e", "f"])
+    inst.myMethodUpperCase(char, alphabet => console.log(`step ${alphabet}`));
+
+
+for (let char of ["g", "h", "i"])
+    inst.myMethod(char, alphabet => console.log(`step ${alphabet}`));
+
+inst.myMethod("j", alphabet => {
+
+    console.log(`completed ${alphabet}`)
+
+    //cSpell: disable
+    console.assert(alphabet === "aBCDEFghij");
+    //cSpell: enable
+
+    console.log("PASS");
+
+});
