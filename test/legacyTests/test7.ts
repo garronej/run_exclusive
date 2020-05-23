f9b2981d6edfe82462c2b12f5670e24c1e90ab40
denoify_ci <denoify_ci@github.com>
2020-05-23 19:17:07 +0000

    Enable shorter import paths [automatic]

---
+++ test/legacyTests/test7.ts
@@ -0,0 +1,92 @@
+import * as runExclusive from "../../lib/runExclusive.ts";
+
+class MyClass {
+
+    constructor() { };
+
+    public alphabet = "";
+
+
+    public myMethod1 = runExclusive.buildMethodCb(runExclusive.createGroupRef(),
+        (char: string, wait: number, callback?: (alphabet: string) => void): void => {
+
+            setTimeout(() => {
+                this.alphabet += char;
+                callback!(this.alphabet);
+            }, wait);
+
+        }
+    );
+
+    public alphabet2 = "";
+
+    public myMethod2 = runExclusive.buildMethodCb(
+        (char: string, wait: number, callback?: (alphabet: string) => void): void => {
+
+            let safeCallback = callback || function () { };
+
+            setTimeout(() => {
+                this.alphabet2 += char;
+                safeCallback(this.alphabet2);
+            }, wait);
+
+        }
+    );
+
+
+}
+
+
+let start = Date.now();
+
+let inst = new MyClass();
+
+inst.myMethod1("a", 1000, alphabet => console.log(alphabet));
+inst.myMethod1("b", 1000, alphabet => console.log(alphabet));
+
+
+let rev = ["n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b"];
+let wait = 500;
+
+for (let char of rev)
+    inst.myMethod2(char, wait, alphabet => console.log(alphabet));
+inst.myMethod2("a", wait, function (this: any) {
+
+    let duration = Date.now() - start;
+
+    //cSpell: disable
+    console.assert(this.alphabet2 === "nmlkjihgfedcba");
+    //cSpell: enable
+
+    let expectedDuration = (rev.length + 1) * wait;
+
+    console.log("expectedDuration: ", expectedDuration);
+    console.log("duration: ", duration);
+
+    console.assert(Math.abs(duration - expectedDuration) < 300);
+    console.assert(duration - expectedDuration >= 0);
+
+    console.log("PASS");
+
+
+});
+
+inst.myMethod1("c", 1000, alphabet => console.log(alphabet));
+inst.myMethod1("d", 1000, () => {
+
+    let duration = Date.now() - start;
+
+    //cSpell: disable
+    console.assert(inst.alphabet === "abcd");
+    //cSpell: enable
+
+    let expectedDuration = 1000 * 4;
+
+    console.log("expectedDuration: ", expectedDuration);
+    console.log("duration: ", duration);
+
+    console.assert(Math.abs(duration - expectedDuration) < 300);
+    console.assert(duration - expectedDuration >= 0);
+
+
+});
---
+++ test/test16.ts
@@ -0,0 +1,60 @@
+import * as runExclusive from "../lib/runExclusive.ts";
+
+let runCount= 0;
+
+export class MyClass{
+
+    constructor() { };
+
+    public myMethod = runExclusive.buildMethod(
+        async (input: string): Promise<[string, number]> => {
+
+            await new Promise<void>(resolve=> setTimeout(()=>resolve(), 1000));
+
+            runCount++;
+
+            return [input + " 0K", 666];
+
+        }
+    );
+
+}
+
+let inst= new MyClass();
+
+(async()=> {
+
+    let [ out1, out2 ]= await inst.myMethod("yo")
+
+    console.assert(out1 === "yo 0K" && out2 === 666 );
+
+    inst.myMethod("ya").then(([out1, out2])=> console.assert(out1 == "ya 0K" && out2 === 666));
+
+    inst.myMethod("foo");
+
+    setTimeout(()=> {
+
+        console.assert(runCount === 2);
+
+    }, 1100);
+
+    setTimeout(()=> {
+
+        console.assert(runCount === 3);
+
+        console.log("DONE");
+
+    }, 3100);
+
+
+})();
+
+
+
+
+
+
+
+
+
+
