92096cecca44643dcc45865394ec982bba114d53
denoify_ci <denoify_ci@github.com>
2022-07-24 18:12:20 +0000

    Enabling shorter import paths [automatic]

---
+++ deno_dist/test/test8.ts
@@ -0,0 +1,89 @@
+import * as runExclusive from "../lib/runExclusive.ts";
+
+class MyClass{
+
+    constructor(){};
+
+    public alphabet= "";
+
+    public myMethod1= runExclusive.buildMethod(
+        async (char: string, wait: number): Promise<string> => {
+
+            await new Promise<void>(resolve=>setTimeout(()=>resolve(),wait));
+
+            this.alphabet+= char;
+
+            return this.alphabet;
+
+    });
+
+    public alphabet2= "";
+
+    public myMethod2= runExclusive.buildMethod(
+        async (char: string, wait: number): Promise<string> => {
+
+            await new Promise<void>(resolve=>setTimeout(()=>resolve(),wait));
+
+            this.alphabet2+= char;
+
+            return this.alphabet2;
+
+    });
+
+}
+
+
+let start= Date.now();
+
+let inst= new MyClass();
+
+inst.myMethod1("a", 1000).then( alphabet=> console.log(alphabet));
+inst.myMethod1("b", 1000).then( alphabet=> console.log(alphabet));
+
+
+let rev= [ "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b" ];
+let wait= 500;
+
+for( let char of rev)
+    inst.myMethod2(char, wait ).then( alphabet => console.log(alphabet));
+inst.myMethod2("a", wait ).then( function() {
+
+    let duration= Date.now() - start;
+
+    //cSpell: disable
+    console.assert(inst.alphabet2 === "nmlkjihgfedcba" );
+    //cSpell: enable
+
+    let expectedDuration= (rev.length+1)*500;
+
+
+    console.log("expectedDuration: ", expectedDuration);
+    console.log("duration: ", duration);
+
+    console.assert( Math.abs(duration - expectedDuration) < 300 );
+    console.assert( duration - expectedDuration >= 0 );
+
+    console.log("PASS");
+
+
+});
+
+inst.myMethod1("c", 1000).then( alphabet=> console.log(alphabet));
+inst.myMethod1("d", 1000).then( ()=>{
+
+    let duration= Date.now() - start;
+
+    //cSpell: disable
+    console.assert(inst.alphabet === "abcd" );
+    //cSpell: enable
+
+    let expectedDuration= 1000*4;
+
+    
+    console.log("expectedDuration: ", expectedDuration);
+    console.log("duration: ", duration);
+
+    console.assert( Math.abs(duration - expectedDuration) < 300 );
+    console.assert( duration - expectedDuration >= 0 );
+
+});
---
+++ test/legacyTests/test17.js
@@ -0,0 +1,35 @@
+"use strict";
+exports.__esModule = true;
+exports.MyClass = void 0;
+//Import ExecStack to be able to export stacked function
+var runExclusive = require("../../lib/runExclusive");
+var ts_evt_1 = require("ts-evt");
+var MyClass = /** @class */ (function () {
+    function MyClass() {
+        var _this = this;
+        this.evtNoCallback = new ts_evt_1.Evt();
+        this.myMethod = runExclusive.buildMethodCb(function (message, callback) {
+            setTimeout(function () {
+                if (!callback.hasCallback)
+                    _this.evtNoCallback.post(message);
+                callback(message);
+            }, 1000);
+        });
+    }
+    ;
+    return MyClass;
+}());
+exports.MyClass = MyClass;
+var inst = new MyClass();
+var success = false;
+inst.evtNoCallback.attach(function (message) {
+    console.assert(message === "noCallback");
+    success = true;
+});
+inst.myMethod("callback", function (message) { return console.assert("callback" === message); });
+inst.myMethod("noCallback");
+setTimeout(function () {
+    console.assert(success);
+    console.log("DONE");
+}, 2500);
+//# sourceMappingURL=test17.js.map
